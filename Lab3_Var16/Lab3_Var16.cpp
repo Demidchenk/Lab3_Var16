0eee4a49d4b5f1061850d34739dd917dbfacd4f3
Алексей <alexey.demidchenko@lll.kpi.ua>
2021-01-14 18:14:37 +0200

    Add project files.

---
+++ Lab3_Var16/Lab3_Var16.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double y[1000], e;
+    int i = 1;
+    y[0] = 0;
+    y[1] = 0.5;
+    cout << "Input e:\n";
+    cin >> e;
+    while (y[i] - y[i - 1] >= e)
+    {
+        i++;
+        y[i] = (y[i - 1] + 1) / (y[i - 1] + 2);
+    }
+    cout << "First correct element is y[" << i << "];\n y[" << i << "] - y[" << i-1 << "] = " << y[i]-y[i-1];
+}
